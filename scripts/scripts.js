ef701e1758ec3dc17e5922033cdcdd4a05fc908b
boyden zhou <boydenz1999@gmail.com>
2023-04-01 17:52:24 -0700

    added hide text area

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,16 +4,16 @@ const notesArray = [
     {title: "note two", body: "this is my second note"}
   ];
   
-  // Get the elements we need
-  const body = document.querySelector('body');
-  const sidebar = document.querySelector('.sidebar');
-  const notetaker = document.querySelector('.notetaker');
-  const darkThemeButton = document.querySelector('.darkThemeButton');
-  const newNoteButton = document.querySelector('.newNoteButton');
-  const saveButton = document.querySelector('.saveButton');
-  const cancelButton = document.querySelector('.cancelButton');
-  const textarea = document.querySelector('#textarea');
-  const sidebarList = document.querySelector('.sidebar ul');
+// Get the elements we need
+const body = document.querySelector('body');
+const sidebar = document.querySelector('.sidebar');
+const notetaker = document.querySelector('.notetaker');
+const darkThemeButton = document.querySelector('.darkThemeButton');
+const newNoteButton = document.querySelector('.newNoteButton');
+const saveButton = document.querySelector('.saveButton');
+const cancelButton = document.querySelector('.cancelButton');
+const textarea = document.querySelector('#textarea');
+const sidebarList = document.querySelector('.sidebar ul');
   
 // Add event listeners
 darkThemeButton.addEventListener('click', toggleDarkTheme);
@@ -28,3 +28,26 @@ function toggleDarkTheme() {
   sidebar.classList.toggle('dark-theme');
   notetaker.classList.toggle('dark-theme');
 }
+
+// Toggle textarea
+function toggleTextArea() {
+    const textareaContainer = document.querySelector('.notetaker .textarea-container');
+    textareaContainer.classList.toggle('hidden');
+    saveButton.classList.toggle('hidden');
+    cancelButton.classList.toggle('hidden');
+    if (!textareaContainer.classList.contains('hidden')) {
+      textarea.focus();
+    }
+    if (newNoteButton.textContent === 'New Note') {
+      textarea.value = '';
+    }
+    newNoteButton.textContent = (newNoteButton.textContent === 'New Note') ? 'Clear' : 'New Note';
+  }
+  
+// Hide textarea
+function hideTextArea() {
+    const textareaContainer = document.querySelector('.notetaker .textarea-container');
+    textareaContainer.classList.add('hidden');
+    saveButton.classList.add('hidden');
+    cancelButton.classList.add('hidden');
+  }
