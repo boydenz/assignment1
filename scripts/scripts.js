025684341c40d7be3f5682325acd07d86398758f
boyden zhou <boydenz1999@gmail.com>
2023-04-01 17:50:30 -0700

    added toggle for dark theme, cried a little more

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -15,64 +15,16 @@ const notesArray = [
   const textarea = document.querySelector('#textarea');
   const sidebarList = document.querySelector('.sidebar ul');
   
-  // Add event listeners
-  darkThemeButton.addEventListener('click', toggleDarkTheme);
-  newNoteButton.addEventListener('click', toggleTextArea);
-  saveButton.addEventListener('click', saveNote);
-  cancelButton.addEventListener('click', hideTextArea);
-  sidebarList.addEventListener('click', displayNote);
-  
-  // Toggle dark theme
-  function toggleDarkTheme() {
-    body.classList.toggle('dark-theme');
-    sidebar.classList.toggle('dark-theme');
-    notetaker.classList.toggle('dark-theme');
-  }
-  
-  // Toggle textarea
-  function toggleTextArea() {
-    const textareaContainer = document.querySelector('.notetaker .textarea-container');
-    textareaContainer.classList.toggle('hidden');
-    saveButton.classList.toggle('hidden');
-    cancelButton.classList.toggle('hidden');
-    if (!textareaContainer.classList.contains('hidden')) {
-      textarea.focus();
-    }
-    if (newNoteButton.textContent === 'New Note') {
-      textarea.value = '';
-    }
-    newNoteButton.textContent = (newNoteButton.textContent === 'New Note') ? 'Clear' : 'New Note';
-  }
-  
-  // Hide textarea
-  function hideTextArea() {
-    const textareaContainer = document.querySelector('.notetaker .textarea-container');
-    textareaContainer.classList.add('hidden');
-    saveButton.classList.add('hidden');
-    cancelButton.classList.add('hidden');
-  }
-  
-  // Save note
-  function saveNote() {
-    const title = prompt('Please enter a title for your note:');
-    if (title !== null && title !== '') {
-      const note = {title: title, body: textarea.value};
-      notesArray.push(note);
-      const listItem = document.createElement('li');
-      const textNode = document.createTextNode(note.title);
-      listItem.appendChild(textNode);
-      sidebarList.appendChild(listItem);
-      textarea.value = '';
-      toggleTextArea();
-    }
-  }
-  
-  // Display note
-  function displayNote(event) {
-    const target = event.target;
-    if (target.tagName === 'LI') {
-      const title = target.textContent;
-      const note = notesArray.find(note => note.title === title);
-      textarea.value = note.body;
-    }
-  }
+// Add event listeners
+darkThemeButton.addEventListener('click', toggleDarkTheme);
+newNoteButton.addEventListener('click', toggleTextArea);
+saveButton.addEventListener('click', saveNote);
+cancelButton.addEventListener('click', hideTextArea);
+sidebarList.addEventListener('click', displayNote);
+
+// Toggle dark theme
+function toggleDarkTheme() {
+  body.classList.toggle('dark-theme');
+  sidebar.classList.toggle('dark-theme');
+  notetaker.classList.toggle('dark-theme');
+}
